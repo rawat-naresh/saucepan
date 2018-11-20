8718c94c68f518e4c78db783a9d02d9eca2bc331
Naresh Rawat <nareshrawat33@gmail.com>
2018-11-20 17:05:13 +0530

    added resizing functionality

--- index.js
+++ index.js
@@ -5,79 +5,119 @@ const fileType = require('file-type');
 const AWS = require('aws-sdk');
 
 function Saucepan(options){
-	if(typeof options != 'object') {
-		throw new TypeError('constructor Saucepan expects an Object as parameter');
-	} else {
-		this.options = options;
+	
+	this.configs = {
+		original:true,
+		sizes:[],
+	};
+
+	if(typeof options != 'object') 
+		throw new TypeError('Saucepan expects an Object as a parameter');
+	else
+		Object.assign(this.configs,options);
+	
+	if(!Buffer.isBuffer(this.configs.buffer)) 
+		throw new TypeError('Expected opts.buffer to be Buffer');
+
+	if(typeof this.configs.original != 'boolean')
+		throw new TypeError('Expected opts.original to be Boolean');	
+
+	if(this.configs.sizes){
+		
+		if(Array.isArray(this.configs.sizes) && this.configs.sizes.length > 0){
+			this.configs.sizes.forEach((item)=>{
+				if(item.path == 'undefined' || item.xy == 'undefined' || isNaN(item.xy) || item.xy < 1 )
+					throw new TypeError('Invalid opts.sizes');	
+			});
+		}
+		else
+			throw new TypeError('Expected opts.sizes to be an Array of sizes');	
 	}
 
-	if(!Buffer.isBuffer(options.buffer)) {
-		throw new TypeError('Expected opts.buffer to be Buffer');
-	} 
+	if(this.configs.original) {
+		this.configs.sizes.push({path:'original',xy:null});
+	}
+
 }
 
 
 Saucepan.prototype.storeToLocal = function(path){
-
-	if(!fs.existsSync(path)){
-		//given path doesn't exists
+	let self = this;
+	if(!fs.existsSync(path))
 		throw new Error(`Path ${path} doesn't exists`);
-    } 
-    
+     
 	const randomKey = getRandomKey();
-	const type = fileType(this.options.buffer);
+	const type = fileType(this.configs.buffer);
 
-	return new Promise((resolve,reject)=>{
-		sharp(this.options.buffer)
-		.toFile(`${path}/${randomKey}.${type.ext}`,function(err,info){
-			if(err){
-				reject(err);
-			}
-			resolve(`${randomKey}.${type.ext}`);			
-		})
-		
-	});
+	const resize = size => sharp(this.configs.buffer)
+	 				.resize(size.xy)
+	 				.toFile(`${path}/${randomKey}-${size.path}.${type.ext}`);
 
+	return Promise.all(this.configs.sizes.map(resize)).then(results =>
+		Promise.resolve(results.map((result,index)=>{
+			return {filename:`${randomKey}-${self.configs.sizes[index].path}.${type.ext}`}
+		}))
+	).catch(err => new Promise.reject(err))
 }
 
 Saucepan.prototype.storeToS3 = function(s3Params){
-	if(typeof s3Params != 'object'){
+	let self = this;
+	if(typeof s3Params != 'object')
 		throw new TypeError('method storeToS3 expects an Object as parameter');
-	}
-	if(!s3Params.accessKeyId) {
+
+	if(!s3Params.accessKeyId) 
 		throw new TypeError('method storeToS3 expects an Object with accessKeyId');
-	}
-	if(!s3Params.secretAccessKey){
+	
+	if(!s3Params.secretAccessKey)
 		throw new TypeError('method storeToS3 expects an Object with secretAccessKey');		
-	}
-	if(!s3Params.bucket){
+	
+	if(!s3Params.bucket)
 		throw new TypeError('method storeToS3 expects an Object with bucket ');		
-	}
+	
 	let  s3Client = new AWS.S3({
 		accessKeyId: s3Params.accessKeyId,
 		secretAccessKey: s3Params.secretAccessKey,
 	  });
 
 	const randomKey = getRandomKey();
-	const type = fileType(this.options.buffer);
+	const type = fileType(this.configs.buffer);
 	const filePath = randomKey+'.'+type.ext;
 
-	return new Promise((resolve,reject)=>{
-		s3Client.putObject({
-			Bucket: s3Params.bucket,
-			Key: `${randomKey}.${type.ext}`,
-			ACL: s3Params.acl || 'public-read',
-			Body: this.options.buffer,
-			ContentType: type.mime,
-		},function(err,data){
-			if(err){
-				reject(err);
-			}
-			resolve(Object.assign(data,{path:filePath}));
-		});
 
-	});
-	
+	const resize = size => sharp(this.configs.buffer)
+	.resize(size.xy)
+	.toBuffer();
+
+	const s3Upload = resizedImage => {
+
+		return new Promise((resolve,reject)=>{
+			s3Client.putObject({
+				Bucket: s3Params.bucket,
+				Key: resizedImage.key,
+				ACL: s3Params.acl || 'public-read',
+				Body: resizedImage.buffer,
+				ContentType: type.mime,
+			},function(err,data){
+				if(err)
+					reject(err);
+				else
+					resolve(Object.assign(data,{path:resizedImage.key}));
+			});
+		});
+	}
+
+	return Promise.all(this.configs.sizes.map(resize)).then(function(results){
+		let resultWithKey = results.map((item,index)=>{
+			return {
+				buffer:item,
+				key:`${randomKey}-${self.configs.sizes[index].path}.${type.ext}`
+			}
+		});
+		return Promise.all(resultWithKey.map(s3Upload))
+	}
+	).then(function(results){
+		return Promise.resolve(results);
+	}).catch(err => Promise.reject(err))	
 }
 
 function getRandomKey(){
